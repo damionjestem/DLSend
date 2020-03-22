6aafc22395e142a87f935a9e68c64bd474a3734b
damionjestem <ndnd@protonmail.ch>
2020-03-22 04:21:33 +0100

    add EmailController

---
+++ DLOverload/Controllers/EmailsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DLOverload.Models;
+
+namespace DLOverload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailsController : ControllerBase
+    {
+        private readonly EmailDBContext _context;
+
+        public EmailsController(EmailDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Emails
+        [HttpGet]
+        public IEnumerable<Email> GetEmails()
+        {
+            return _context.Emails;
+        }
+
+        // GET: api/Emails/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmail([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var email = await _context.Emails.FindAsync(id);
+
+            if (email == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(email);
+        }
+
+        // PUT: api/Emails/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmail([FromRoute] string id, [FromBody] Email email)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != email.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(email).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Emails
+        [HttpPost]
+        public async Task<ActionResult<Email>> PostEmail(Email email)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Emails.Add(email);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmail), new { id = email.Id }, email);
+        }
+
+        // DELETE: api/Emails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmail([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var email = await _context.Emails.FindAsync(id);
+            if (email == null)
+            {
+                return NotFound();
+            }
+
+            _context.Emails.Remove(email);
+            await _context.SaveChangesAsync();
+
+            return Ok(email);
+        }
+
+        private bool EmailExists(string id)
+        {
+            return _context.Emails.Any(e => e.Id == id);
+        }
+    }
+}
